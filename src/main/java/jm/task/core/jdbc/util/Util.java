9b340db183d511313ad40a6aadcd29fe9ce4ed08
klerua-pe <a034871vz@gmail.com>
2020-09-16 13:06:46 +0300

    Закончил

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String LOGIN = "root";
+    private static final String PASS = "root";
+
+    public static Connection getConnection() {
+        regDriver();
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASS);
+            //if (!connection.isClosed()) System.out.println("Успешное Подключение к бд");
+            if (connection.isClosed()) System.out.println("Соединение с бд закрыто");
+            return connection;
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка в классе Util", e);
+        }
+    }
+    private static void regDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
